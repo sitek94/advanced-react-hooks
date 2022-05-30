27f5a5932da268a0b5f260d17cbb7e4607863d4f
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2022-05-30 05:58:08 +0200

    ex 2.3 - prevent memory leaks (React < v18) with safeDispatch

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -28,13 +28,32 @@ function asyncReducer(state, action) {
   }
 }
 
+function useSafeDispatch(dispatch) {
+  const mountedRef = React.useRef(false)
+
+  React.useEffect(() => {
+    mountedRef.current = true
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  return React.useCallback(
+    (...args) => (mountedRef.current ? dispatch(...args) : void 0),
+    [dispatch],
+  )
+}
+
 function useAsync(initialState) {
-  const [state, dispatch] = React.useReducer(asyncReducer, {
+  const [state, unsafeDispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
     error: null,
     ...initialState,
   })
+
+  const dispatch = useSafeDispatch(unsafeDispatch)
+
   const run = React.useCallback(promise => {
     if (!promise) {
       return
